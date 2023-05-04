1b105814eb856a85b65842fe2f7d6a35abcab543
Paulo Khayat <paulokhayat@gmail.com>
2023-05-04 15:17:04 +0300

    Update Joystick class to be abstract

--- Ktane_Code/Joystick.cpp
+++ Ktane_Code/Joystick.cpp
@@ -7,6 +7,9 @@ class Joystick {
     int _pin3;
     int _pin4;
     int currVal = B0000;
+  public:
+  byte getPos() = 0;
+  byte getRaw() = 0;
 };
 class Joystick_TwoWay : public Joystick {
   private:
@@ -19,7 +22,7 @@ class Joystick_TwoWay : public Joystick {
 
     }
 
-    byte getPos() {
+    byte getPos() override {
       currVal = digitalRead(_pin1) * 4 + digitalRead(_pin2) * 2 + digitalRead(_pin3);
       for (int l = 0; l < 7; l++) {
         if (list[l] == currVal)
@@ -27,7 +30,7 @@ class Joystick_TwoWay : public Joystick {
       }
       return 0;
     }
-    byte getRaw() {
+    byte getRaw() override {
       currVal = digitalRead(_pin1) * 4 + digitalRead(_pin2) * 2 + digitalRead(_pin3);
       return currVal;
     }
@@ -44,7 +47,7 @@ class Joystick_Lever : public Joystick {
       _pin4 = pin4;
     }
 
-    byte getPos() {
+    byte getPos() override {
       currVal = digitalRead(_pin1) * 8 + digitalRead(_pin2) * 4 + digitalRead(_pin3) * 2 + digitalRead(_pin4);
       for (int l = 0; l < 15; l++) {
         if (list[l] == currVal)
@@ -52,7 +55,7 @@ class Joystick_Lever : public Joystick {
       }
       return 0;
     }
-    byte getRaw() {
+    byte getRaw() override {
       currVal = digitalRead(_pin1) * 8 + digitalRead(_pin2) * 4 + digitalRead(_pin3) * 2 + digitalRead(_pin4);
       return currVal;
     }
